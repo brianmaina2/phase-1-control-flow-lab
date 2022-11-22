f90ec8ea3ae415811376060bddbab037d82cbef7
brian.maina@student.moringaschool.com <brianmaina2>
2022-11-22 18:13:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400){
+    return 'This one is on me!';
 }
+else if (someNumber > 2000 && someNumber <2500){
+  return 'I will gladly take your thirty bucks.';}
+  else if(someNumber > 2500){
+    return  'No can do.';
+  }
 
+}
 function ternaryCheckCity(){
-  // Write your code here!
+  if ('city' === "NYC"){
+    return "ok, sounds good.";
+  }
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip ==="generous"){
+    return "Thank you so much.";
+  }
+  else if (tip === "not as generous"){
+    return "Thank you.";
+  }
+  else{
+    return "Bye.";
+  }
 }
