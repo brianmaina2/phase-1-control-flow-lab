a1e86a96876f093a78ba703ae5fe55138beb7cb3
brian.maina@student.moringaschool.com <brianmaina2>
2022-11-24 21:21:49 +0300

    commit tree 03ecbad513a4559e7325877442410334bad41e13

--- test/helpers.js
+++ test/helpers.js
@@ -1,20 +1,32 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
-
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
-
-const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
+function distanceFromHqInBlocks(someValue) {
+  if (someValue<42){
+    return(42-someValue);
   }
-);
+  else{
+    return(someValue-42);
+  }
+}
+function distanceFromHqInFeet(someValue) {
+  distanceFromHqInBlocks(someValue)*264;
+}
+function distanceTravelledInFeet(a,b){
+ if (b>a){
+   return((b-a)*264);
+}
+else
+   return((a-b)*264);
 
-const src = babelResult.code
+}
 
-jsdom({
-  html, src
-});
+function calculateFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start,destination);
+  if (distance <=400){ 
+    return 0
+  }else if(distance >400 && distance <2000){
+    return 0.02
+  }else if(distance > 2000 && distance < 2500){
+    return 25
+  }else if(distance>2500){
+    return'cannot travel that far'
+  }
+}
