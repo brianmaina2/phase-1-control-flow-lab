5cbb3cb565aca7f243292cbe1963a6aff8f11188
brian.maina@student.moringaschool.com <brianmaina2>
2022-11-24 16:38:19 +0300

    comlited assignment

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-function scuberGreetingForFeet(someNumber){
-  if (someNumber <= 400){
-    return 'This one is on me!';
-}
-else if (someNumber > 2000 && someNumber <2500){
-  return 'I will gladly take your thirty bucks.';}
-  else if(someNumber > 2500){
-    return  'No can do.';
-  }
+function scuberGreetingForFeet(distance){
 
+  if (distance<=400){
+  return 'This one is on me!';
+
+}else if (distance > 2000 && distance < 2500){
+  return 'I will gladly take your thirty bucks.';{}}
+   else if (distance > 2500){
+    return 'No can do.';
 }
-function ternaryCheckCity(){
-  if ('city' === "NYC"){
-    return "ok, sounds good.";
-  }
 }
 
-function switchOnCharmFromTip(){
-  if (tip ==="generous"){
-    return "Thank you so much.";
-  }
-  else if (tip === "not as generous"){
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    
+      
+      // break;
+  case 'not as generous':
     return "Thank you.";
-  }
-  else{
-    return "Bye.";
+  
+    default:
+      return "Bye.";
   }
 }
